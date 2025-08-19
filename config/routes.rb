ee8403a4c1b2b227593dfcf029f2376700a4f2f4
douglas rocha <douglashidekazu15@gmail.com>
2025-08-19 12:29:32 +0900

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :chats, only: [:index, :show, :new, :create, :destroy] do
+    resources :messages, only: [:create]
+  end
 
 
   # Defines the root path route ("/")
