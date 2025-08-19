7f455c2ff3ca38db7e480538b1fed572728ca009
douglas rocha <douglashidekazu15@gmail.com>
2025-08-19 17:23:58 +0900

    added llm doenv gem created ask llm

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,33 @@
 class MessagesController < ApplicationController
   def create
     @chat = Chat.find(params[:chat_id])
-    @message.chat = @chat
     @message = Message.new(message_params)
+    @message.chat = @chat
     if @message.save
-      redirect_to chat_path(@chat)
       prompt = <<-PROMPT
-                  You are an assistant specialized in professional email communication.
+      You are an assistant specialized in professional email communication.
 
-                  I'm new at work and I want to learn how to write this #{@message.content} properly
-                  depending on this level of politeness #{@chat.politeness}.
+      I'm new at work and I want to learn how to write this #{@message.content} properly
+      depending on this level of politeness #{@chat.politeness}.
 
-                  Guide me into how to write the perfect email depending on who i'm talking to
+      Guide me into how to write the perfect email depending on who i'm talking to
 
-                  show me your version of the email marking the difference between both.
-                PROMPT
+      show me your version of the email marking the difference between both.
+      PROMPT
 
       @response = RubyLLM.chat.ask(prompt)
+      messageAi = Message.new(message_params)
+      messageAi.content = @response.content
+      messageAi.who_sent = "ai"
+      messageAi.chat = @chat
+      @chats = Chat.all
+      if messageAi.save
+        return redirect_to chat_path(@chat)
+      else
+        return render 'chats/show', status: :unprocessable_content
+      end
 
+      redirect_to chat_path(@chat)
     else
       render 'chats/show', status: :unprocessable_content
     end
