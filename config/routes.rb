e6976b181ac9126cb73f715b33bf994cb97b2b4d
Rafael Pradillo <tasfa66@gmail.com>
2025-08-20 15:04:35 +0900

    reutes to main page now differenciates from author ized and unauthorized users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "devise/sessions#new"
+    authenticated :user do
+    root to: "chats#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
   get "up" => "rails/health#show", as: :rails_health_check
   resources :chats, only: [:index, :show, :create, :destroy] do
     resources :messages, only: [:create]
