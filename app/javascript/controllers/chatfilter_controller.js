cbb8c0df29fa834d1bcc3680baafc5f1ff7a956b
Michael Henderson <m.nishimoto97@gmail.com>
2025-08-21 17:23:11 +0900

    Created a filter function for the chats

---
+++ app/javascript/controllers/chatfilter_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="chatfilter"
+export default class extends Controller {
+  static targets = ["searchbar", "searchchats"]
+  connect() {
+  }
+
+  filter() {
+    const searchTerm = this.searchbarTarget.value;
+    console.log("Okay seaching here");
+    this.searchchatsTargets.forEach((chat) => {
+      const receiver = chat.getAttribute("data-key");
+      const isMatch = receiver.includes(searchTerm);
+      chat.style.display = isMatch ? "" : "none";
+    })
+  }
+}
