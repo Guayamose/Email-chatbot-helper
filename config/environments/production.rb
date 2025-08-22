97c240faf703503965c7825af214dd1815684d96
kaleb fukuda <kalebfukudajp@gmail.com>
2025-08-22 15:57:14 +0900

    Added gmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,12 +75,22 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  # config.action_mailer.smtp_settings = {
+  #   user_name: ENV['SMTP_USER'],      # Mailtrap sandbox username
+  #   password: ENV['SMTP_PASSWORD'],   # Mailtrap sandbox password
+  #   address: 'smtp.mailtrap.io',
+  #   port: 2525,
+  #   authentication: :login,
+  #   enable_starttls_auto: true
+  # }
+
   config.action_mailer.smtp_settings = {
-    user_name: ENV['SMTP_USER'],      # Mailtrap sandbox username
-    password: ENV['SMTP_PASSWORD'],   # Mailtrap sandbox password
-    address: 'smtp.mailtrap.io',
-    port: 2525,
-    authentication: :login,
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: :plain,
+    user_name: ENV['GMAIL_USERNAME'],        # your Gmail address
+    password: ENV['GMAIL_APP_PASSWORD'],     # the app password
     enable_starttls_auto: true
   }
 
